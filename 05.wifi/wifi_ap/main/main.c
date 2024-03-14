23efd823aee2b4f62798ffab00405de15857ef24
duruofu <duruofu@qq.com>
2024-03-14 22:04:36 +0800

    update

--- 05.wifi/wifi_ap/main/main.c
+++ 05.wifi/wifi_ap/main/main.c
@@ -1,21 +1,30 @@
-#include <stng.h>
+#include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/event_groups.h"
 #include "esp_wifi.h"
 #include "esp_log.h"
 #include "esp_event.h"
 #include "nvs_flash.h"
+#include "esp_mac.h"
 
 #define ESP_WIFI_AP_SSID "DuRuofu_ESP32"
 #define ESP_WIFI_AP_PASSWD "3.1415926"
 
 void WIFI_CallBack(void *event_handler_arg,esp_event_base_t event_base,int32_t event_id,void *event_data)
 {
+	// 连接事件
 	if(event_base == IP_EVENT && event_id == IP_EVENT_AP_STAIPASSIGNED)
 	{
 		// 解析数据
-		ip_event_got_ip_t *event_info = (ip_event_got_ip_t *)event_data;
-		ESP_LOGI("WIFI_AP", "Got IP:" IPSTR, IP2STR(&event_info->ip_info.ip));
+		ip_event_ap_staipassigned_t* event_info = (ip_event_ap_staipassigned_t *)event_data;
+		ESP_LOGW("WIFI_AP", "设备已连接 MAC:"MACSTR"", MAC2STR(event_info->mac));
+	}
+
+	// 断开连接事件
+	if(event_base == WIFI_EVENT && event_id == WIFI_EVENT_AP_STADISCONNECTED)
+	{
+		wifi_event_ap_stadisconnected_t* event_info = (wifi_event_ap_stadisconnected_t*)event_data;
+		ESP_LOGW("WIFI_AP", "设备已断开 MAC:"MACSTR"", MAC2STR(event_info->mac));
 	}
 }
 
@@ -37,7 +46,10 @@ void app_main(void)
 
 	// 初始化默认事件循环
 	ESP_ERROR_CHECK(esp_event_loop_create_default());
+	// 注册设备连接事件回调
 	esp_event_handler_instance_register(IP_EVENT, IP_EVENT_AP_STAIPASSIGNED, WIFI_CallBack, NULL, NULL);
+	// 注册设备断开连接设备回调
+	esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_AP_STADISCONNECTED, WIFI_CallBack, NULL, NULL);
 
 	// *esp_netif_ap 可以用来修改AP设置
 	esp_netif_t *esp_netif_ap = esp_netif_create_default_wifi_ap();
