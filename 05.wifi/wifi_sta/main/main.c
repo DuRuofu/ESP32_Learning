514b85dbf539e5104c1d7c780fcc8c5b4a3802aa
duruofu <duruofu@qq.com>
2024-03-15 08:55:11 +0800

    add wifi_sta

--- 05.wifi/wifi_sta/main/main.c
+++ 05.wifi/wifi_sta/main/main.c
@@ -1,6 +1,95 @@
-#include <stdio.h>
+#includ<stdio.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "esp_mac.h"
+
+#define ESP_WIFI_STA_SSID "duruofu_win10"
+#define ESP_WIFI_STA_PASSWD "1234567890"
+
+void WIFI_CallBack(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+	static uint8_t connect_count = 0;
+	// WIFI 启动成功
+	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+	{
+		ESP_LOGI("WIFI_EVENT", "WIFI_EVENT_STA_START");
+		ESP_ERROR_CHECK(esp_wifi_connect());
+	}
+	// WIFI 连接失败
+	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+	{
+		ESP_LOGI("WIFI_EVENT", "WIFI_EVENT_STA_DISCONNECTED");
+		connect_count++;
+		if (connect_count < 6)
+		{
+			vTaskDelay(1000 / portTICK_PERIOD_MS);
+			ESP_ERROR_CHECK(esp_wifi_connect());
+		}
+		else{
+			ESP_LOGI("WIFI_EVENT", "WIFI_EVENT_STA_DISCONNECTED 10 times");
+		}
+	}
+	// WIFI 连接成功(获取到了IP)
+	if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+	{
+		ESP_LOGI("WIFI_EVENT", "WIFI_EVENT_STA_GOT_IP");
+		ip_event_got_ip_t *info = (ip_event_got_ip_t *)event_data;
+		ESP_LOGI("WIFI_EVENT", "got ip:" IPSTR "", IP2STR(&info->ip_info.ip));
+	}
+}
 
 void app_main(void)
 {
+	//----------------准备阶段-------------------
+	// Initialize NVS
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+	{
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+
+	//----------------初始化阶段-------------------
+	ESP_ERROR_CHECK(esp_netif_init());
+
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+	// 注册事件(wifi启动成功)
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_START, WIFI_CallBack, NULL, NULL));
+	// 注册事件(wifi连接失败)
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, WIFI_CallBack, NULL, NULL));
+	// 注册事件(wifi连接失败)
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, WIFI_CallBack, NULL, NULL));
+
+	// 初始化STA设备
+	esp_netif_create_default_wifi_sta();
+
+	/*Initialize WiFi */
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	// WIFI_INIT_CONFIG_DEFAULT 是一个默认配置的宏
+
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+	//----------------配置阶段-------------------
+	// 设置为STA模式
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+
+	// STA详细配置
+	wifi_config_t sta_config = {
+		.sta = {
+			.ssid = ESP_WIFI_STA_SSID,
+			.password = ESP_WIFI_STA_PASSWD,
+			.bssid_set = false,
+		},
+	};
+	ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta_config));
+
+	//----------------启动阶段-------------------
+	ESP_ERROR_CHECK(esp_wifi_start());
 
 }
