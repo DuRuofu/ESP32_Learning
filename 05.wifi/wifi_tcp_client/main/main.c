b63cb6d92d9cca3e6e0b74e62a186bc230a973c3
duruofu <duruofu@qq.com>
2024-03-15 16:28:34 +0800

    add wifi_tcp_server

--- 05.wifi/wifi_tcp_client/main/main.c
+++ 05.wifi/wifi_tcp_client/main/main.c
@@ -12,7 +12,7 @@
 
 #define ESP_WIFI_STA_SSID "duruofu_win10"
 #define ESP_WIFI_STA_PASSWD "1234567890"
-
+#define TCP_SREVER_ADDR "192.168.137.1"
 #define TCP_SREVER_PORT 8080
 
 static const char *TAG = "main";
@@ -50,126 +50,73 @@ void WIFI_CallBack(void *event_handler_arg, esp_event_base_t event_base, int32_t
 	}
 }
 
-static void do_retransmit(const int sock)
+static void tcp_client_task(void *pvParameters)
 {
-	int len;
-	char rx_buffer[128];
 
-	do
-	{
-		len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-		if (len < 0)
-		{
-			ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
-		}
-		else if (len == 0)
-		{
-			ESP_LOGW(TAG, "Connection closed");
-		}
-		else
-		{
-			rx_buffer[len] = 0; // Null-terminate whatever is received and treat it like a string
-			ESP_LOGI(TAG, "Received %d bytes: %s", len, rx_buffer);
 
-			// send() can return less bytes than supplied length.
-			// Walk-around for robust implementation.
-			int to_write = len;
-			while (to_write > 0)
-			{
-				int written = send(sock, rx_buffer + (len - to_write), to_write, 0);
-				if (written < 0)
-				{
-					ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
-					// Failed to retransmit, giving up
-					return;
-				}
-				to_write -= written;
-			}
-		}
-	} while (len > 0);
-}
+	// 等待wifi连接成功(暂时这样处理)
+	vTaskDelay(5000 / portTICK_PERIOD_MS);
 
-// tcp服务器任务
-static void tcp_server_task(void *pvParameters)
-{
-	// 创建套接字
-	int listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (listen_sock < 0)
+	ESP_LOGI("tcp_client_task", "tcp_client_task start");
+
+	// 创建socket
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	if (sock < 0) // 创建失败返回-1
 	{
 		ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
-		vTaskDelete(NULL);
 		return;
 	}
-	// 设置套接字属性
-	int opt = 1;
-	setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-	ESP_LOGI(TAG, "Socket created");
 
 	// 设置服务器(IPV4)
-	struct sockaddr_storage dest_addr;
-	struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *)&dest_addr;
-	dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
-	dest_addr_ip4->sin_family = AF_INET;
-	dest_addr_ip4->sin_port = htons(TCP_SREVER_PORT);
+	struct sockaddr_in server_config;
+	server_config.sin_addr.s_addr = inet_addr(TCP_SREVER_ADDR);
+	server_config.sin_family = AF_INET;
+	server_config.sin_port = htons(TCP_SREVER_PORT); // 宏htons 用于将主机的无符号短整型数据转换成网络字节顺序(小端转大端)
 
-	// 绑定套接字
-	int err = bind(listen_sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+	// 连接服务器
+	int err = connect(sock, (struct sockaddr *)&server_config, sizeof(server_config));
 	if (err != 0)
 	{
-		ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
-		ESP_LOGE(TAG, "IPPROTO: %d", AF_INET);
-		goto CLEAN_UP;
-	}
-	ESP_LOGI(TAG, "Socket bound, port %d", TCP_SREVER_PORT);
-
-	// 监听套接字 (阻塞)
-	err = listen(listen_sock, 1);
-	if (err != 0)
-	{
-		ESP_LOGE(TAG, "Error occurred during listen: errno %d", errno);
-		goto CLEAN_UP;
+		ESP_LOGE(TAG, "Socket unable to connect: errno %d", errno);
+		return;
 	}
 
-	while (1)
+	// 发送数据
+	const char *data = "Hello World!";
+	int len = send(sock, data, strlen(data), 0);
+	if (len < 0)
 	{
-		char addr_str[128];
+		ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+		return;
+	}
 
-		ESP_LOGI(TAG, "Socket listening");
-		struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
-		socklen_t addr_len = sizeof(source_addr);
-		int sock = accept(listen_sock, (struct sockaddr *)&source_addr, &addr_len);
-		if (sock < 0)
+	char rx_buffer[1024];
+	// 接收数据,并发回
+	while(1)
+	{
+		int len = recv(sock, rx_buffer, sizeof(rx_buffer), 0);
+		// Error occurred during receiving
+		if (len < 0)
 		{
-			ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
+			ESP_LOGE(TAG, "recv failed: errno %d", errno);
 			break;
 		}
-
-		// Set tcp keepalive option
-		int keepAlive = 1;
-		int keepIdle = 5;	  // TCP keep-alive idle time(s)
-		int keepInterval = 5; // TCP keep-alive interval time(s)
-		int keepCount = 3;	  // TCP keep-alive packet retry send counts
-
-		setsockopt(sock, SOL_SOCKET, SO_KEEPALIVE, &keepAlive, sizeof(int));
-		setsockopt(sock, IPPROTO_TCP, TCP_KEEPIDLE, &keepIdle, sizeof(int));
-		setsockopt(sock, IPPROTO_TCP, TCP_KEEPINTVL, &keepInterval, sizeof(int));
-		setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, &keepCount, sizeof(int));
-		// Convert ip address to string
-		if (source_addr.ss_family == PF_INET)
+		// Data received
+		else
 		{
-			inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
+			ESP_LOGI(TAG, "Received %d bytes from %s:", len, TCP_SREVER_ADDR);
+			ESP_LOGI(TAG, "%.*s", len, rx_buffer);
+
+			// 发送数据
+			int len_end = send(sock, rx_buffer, len, 0);
+			if (len_end < 0)
+			{
+				ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+				break;
+			}
 		}
-
-		ESP_LOGI(TAG, "Socket accepted ip address: %s", addr_str);
-
-		do_retransmit(sock);
-
-		shutdown(sock, 0);
-		close(sock);
 	}
 
-CLEAN_UP:
-	close(listen_sock);
 	vTaskDelete(NULL);
 }
 
@@ -233,6 +180,6 @@ void app_main(void)
 	// 配置启动WIFI
 	wifi_sta_init();
 
-	// 创建TCP服务器任务
-	xTaskCreate(tcp_server_task, "tcp_server_task", 4096, NULL, 5, NULL);
+	// 创建TCP客户端任务
+	xTaskCreate(tcp_client_task, "tcp_client_task", 4096, NULL, 5, NULL);
 }
