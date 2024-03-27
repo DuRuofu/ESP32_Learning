5a37754c9836a8e4e7ff88485a2e45d16e6b3a77
duruofu <duruofu@qq.com>
2024-03-27 12:35:00 +0800

    add mqtt_tcp

---
+++ 05.wifi/mqtt/mqtt_tcp/main/main.c
@@ -0,0 +1,228 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "esp_wifi.h"
+#include "esp_log.h"
+#include "esp_event.h"
+#include "nvs_flash.h"
+#include "esp_mac.h"
+#include "esp_netif.h"
+#include <sys/socket.h>
+#include "esp_eth.h"
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+#include "mqtt_client.h"
+
+
+// 要连接的WIFI
+#define ESP_WIFI_STA_SSID "duruofu_win10"
+#define ESP_WIFI_STA_PASSWD "1234567890"
+
+static const char *TAG = "main";
+
+
+
+void WIFI_CallBack(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+	static uint8_t connect_count = 0;
+	// WIFI 启动成功
+	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+	{
+		ESP_LOGI("WIFI_EVENT", "WIFI_EVENT_STA_START");
+		ESP_ERROR_CHECK(esp_wifi_connect());
+	}
+	// WIFI 连接失败
+	if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+	{
+		ESP_LOGI("WIFI_EVENT", "WIFI_EVENT_STA_DISCONNECTED");
+		connect_count++;
+		if (connect_count < 6)
+		{
+			vTaskDelay(1000 / portTICK_PERIOD_MS);
+			ESP_ERROR_CHECK(esp_wifi_connect());
+		}
+		else
+		{
+			ESP_LOGI("WIFI_EVENT", "WIFI_EVENT_STA_DISCONNECTED 10 times");
+		}
+	}
+	// WIFI 连接成功(获取到了IP)
+	if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+	{
+		ESP_LOGI("WIFI_EVENT", "WIFI_EVENT_STA_GOT_IP");
+		ip_event_got_ip_t *info = (ip_event_got_ip_t *)event_data;
+		ESP_LOGI("WIFI_EVENT", "got ip:" IPSTR "", IP2STR(&info->ip_info.ip));
+	}
+}
+
+// wifi初始化
+static void wifi_sta_init(void)
+{
+	ESP_ERROR_CHECK(esp_netif_init());
+
+	// 注册事件(wifi启动成功)
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_START, WIFI_CallBack, NULL, NULL));
+	// 注册事件(wifi连接失败)
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, WIFI_CallBack, NULL, NULL));
+	// 注册事件(wifi连接失败)
+	ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, WIFI_CallBack, NULL, NULL));
+
+	// 初始化STA设备
+	esp_netif_create_default_wifi_sta();
+
+	/*Initialize WiFi */
+	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+	// WIFI_INIT_CONFIG_DEFAULT 是一个默认配置的宏
+
+	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+	//----------------配置阶段-------------------
+	// 初始化WIFI设备( 为 WiFi 驱动初始化 WiFi 分配资源，如 WiFi 控制结构、RX/TX 缓冲区、WiFi NVS 结构等，这个 WiFi 也启动 WiFi 任务。必须先调用此API，然后才能调用所有其他WiFi API)
+	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+
+	// STA详细配置
+	wifi_config_t sta_config = {
+			.sta = {
+					.ssid = ESP_WIFI_STA_SSID,
+					.password = ESP_WIFI_STA_PASSWD,
+					.bssid_set = false,
+			},
+	};
+	ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta_config));
+
+	//----------------启动阶段-------------------
+	ESP_ERROR_CHECK(esp_wifi_start());
+
+	//----------------配置省电模式-------------------
+	// 不省电(数据传输会更快)
+	ESP_ERROR_CHECK(esp_wifi_set_ps(WIFI_PS_NONE));
+}
+
+static void log_error_if_nonzero(const char *message, int error_code)
+{
+	if (error_code != 0)
+	{
+		ESP_LOGE(TAG, "Last error %s: 0x%x", message, error_code);
+	}
+}
+
+
+// MQTT客户端事件处理
+/*
+ * @brief Event handler registered to receive MQTT events
+ *
+ *  This function is called by the MQTT client event loop.
+ *
+ * @param handler_args user data registered to the event.
+ * @param base Event base for the handler(always MQTT Base in this example).
+ * @param event_id The id for the received event.
+ * @param event_data The data for the event, esp_mqtt_event_handle_t.
+ */
+static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
+{
+	ESP_LOGD(TAG, "Event dispatched from event loop base=%s, event_id=%" PRIi32 "", base, event_id);
+	esp_mqtt_event_handle_t event = event_data;
+	esp_mqtt_client_handle_t client = event->client;
+	int msg_id;
+	switch ((esp_mqtt_event_id_t)event_id)
+	{
+	// MQTT连接成功
+	case MQTT_EVENT_CONNECTED:
+		ESP_LOGI(TAG, "MQTT_EVENT_CONNECTED");
+		// 发布消息
+		msg_id = esp_mqtt_client_publish(client, "/topic/qos1", "data_3", 0, 1, 0);
+		ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+		// 订阅消息
+		msg_id = esp_mqtt_client_subscribe(client, "/topic/qos0", 0);
+		ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+		msg_id = esp_mqtt_client_subscribe(client, "/topic/qos1", 1);
+		ESP_LOGI(TAG, "sent subscribe successful, msg_id=%d", msg_id);
+		// 取消订阅消息
+		msg_id = esp_mqtt_client_unsubscribe(client, "/topic/qos1");
+		ESP_LOGI(TAG, "sent unsubscribe successful, msg_id=%d", msg_id);
+		break;
+	// MQTT连接断开
+	case MQTT_EVENT_DISCONNECTED:
+		ESP_LOGI(TAG, "MQTT_EVENT_DISCONNECTED");
+		break;
+	// MQTT订阅成功
+	case MQTT_EVENT_SUBSCRIBED:
+		ESP_LOGI(TAG, "MQTT_EVENT_SUBSCRIBED, msg_id=%d", event->msg_id);
+		// 发布消息
+		msg_id = esp_mqtt_client_publish(client, "/topic/qos0", "data", 0, 0, 0);
+		ESP_LOGI(TAG, "sent publish successful, msg_id=%d", msg_id);
+		break;
+	// MQTT取消订阅成功
+	case MQTT_EVENT_UNSUBSCRIBED:
+		ESP_LOGI(TAG, "MQTT_EVENT_UNSUBSCRIBED, msg_id=%d", event->msg_id);
+		break;
+	// MQTT发布成功
+	case MQTT_EVENT_PUBLISHED:
+		ESP_LOGI(TAG, "MQTT_EVENT_PUBLISHED, msg_id=%d", event->msg_id);
+		break;
+	// MQTT收到数据
+	case MQTT_EVENT_DATA:
+		ESP_LOGI(TAG, "MQTT_EVENT_DATA");
+		printf("TOPIC=%.*s\r\n", event->topic_len, event->topic);
+		printf("DATA=%.*s\r\n", event->data_len, event->data);
+		break;
+	// MQTT错误
+	case MQTT_EVENT_ERROR:
+		ESP_LOGI(TAG, "MQTT_EVENT_ERROR");
+		if (event->error_handle->error_type == MQTT_ERROR_TYPE_TCP_TRANSPORT)
+		{
+			log_error_if_nonzero("reported from esp-tls", event->error_handle->esp_tls_last_esp_err);
+			log_error_if_nonzero("reported from tls stack", event->error_handle->esp_tls_stack_err);
+			log_error_if_nonzero("captured as transport's socket errno", event->error_handle->esp_transport_sock_errno);
+			ESP_LOGI(TAG, "Last errno string (%s)", strerror(event->error_handle->esp_transport_sock_errno));
+		}
+		break;
+	default:
+		ESP_LOGI(TAG, "Other event id:%d", event->event_id);
+		break;
+	}
+}
+
+// MQTT客户端
+static void mqtt_app_start(void)
+{
+	esp_mqtt_client_config_t mqtt_cfg = {
+			.broker.address.uri = "mqtt://www.duruofu.top:1883",
+	};
+
+	esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
+	/* The last argument may be used to pass data to the event handler, in this example mqtt_event_handler */
+	// 注册事件处理函数
+	esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
+	// 启动MQTT客户端
+	esp_mqtt_client_start(client);
+}
+
+void app_main(void)
+{
+
+	// Initialize NVS
+	esp_err_t ret = nvs_flash_init();
+	if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+	{
+		ESP_ERROR_CHECK(nvs_flash_erase());
+		ret = nvs_flash_init();
+	}
+	ESP_ERROR_CHECK(ret);
+
+	// 创建默认事件循环
+	ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+	// 配置启动WIFI
+	wifi_sta_init();
+
+	// 等待wifi连接成功(暂时这样处理)
+	vTaskDelay(5000 / portTICK_PERIOD_MS);
+
+	// 创建MQTT客户端
+	mqtt_app_start();
+}
